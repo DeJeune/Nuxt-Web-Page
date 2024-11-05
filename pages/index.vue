695079a133becfff6af3582705bce429ffc23c9d
Ubuntu <ubuntu@ip-172-31-31-50.ap-southeast-1.compute.internal>
2024-11-05 15:20:18 +0000

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex items-center justify-center min-h-screen bg-gradient-to-r from-purple-500 to-pink-500">
+    <div class="container mx-auto text-center">
+      <h1 class="text-4xl font-bold text-white">Individual Developer Jeune Astre</h1>
+      <p class="text-xl text-gray-300">We are developing some tools for fun!</p>
+      <div class="flex mt-4 items-center justify-center space-x-4">
+        <a href="https://x.com/shadowfran209">
+          <UIcon name="i-grommet-icons:x" />
+        </a>
+        <a href="https://github.com/DeJeune">
+          <UIcon name="i-grommet-icons:github" />
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
